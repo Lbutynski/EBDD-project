e276df65c8baef3393b8f8ebdd7b086405c61554
Lbutynski <lucas.butynski@supdevinci-edu.fr>
2025-01-18 17:47:06 +0100

    feat: add db validation

--- create_db.sql
+++ create_db.sql
@@ -7,10 +7,10 @@ CREATE TABLE IF NOT EXISTS categories (
     
 CREATE TABLE if not exists produits (
 	id INT auto_increment primary key,
-  nom VARCHAR(100) ,
-  prix double ,
-  quantité_en_stock int,
-  categorie_id int,
+  nom VARCHAR(100) NOT NULL,
+  prix double NOT NULL,
+  quantité_en_stock int NOT NULL DEFAULT 0,
+  categorie_id int NOT NULL,
   foreign key(categorie_id) references categories(id)) ;
 
 CREATE TABLE IF NOT EXISTS fournisseurs(
@@ -19,26 +19,26 @@ CREATE TABLE IF NOT EXISTS fournisseurs(
     
 CREATE TABLE IF NOT exists clients(
 	id int auto_increment primary key,
-  prenom varchar(50),
-  nom varchar(50),
+  prenom varchar(50) NOT NULL,
+  nom varchar(50) NOT NULL,
   addresse varchar(100));
     
 CREATE TABLE IF NOT EXISTS commandes(
 	id int auto_increment primary key,
-  id_client int,
+  id_client int NOT NULL,
   foreign key (id_client) references clients(id));
   
 CREATE table if not exists produits_commandes(
 	id int auto_increment primary key,
-  id_commande int,
-  id_produit int,
+  id_commande int NOT NULL,
+  id_produit int NOT NULL,
   foreign key (id_commande)references commandes(id),
   foreign key (id_produit)references produits(id));
     
 CREATE table if not exists produits_fournisseurs(
 	id int auto_increment primary key,
-  id_produit int,
-  id_fournisseur int,
+  id_produit int NOT NULL,
+  id_fournisseur int NOT NULL,
   foreign key (id_produit)references produits(id),
   foreign key (id_fournisseur)references fournisseurs(id));
     
