f37dc2952a09b8bef90c412048d0a6cc6510578c
Lbutynski <lucas.butynski@supdevinci-edu.fr>
2025-01-18 18:35:37 +0100

    feat: add commande by client search

--- routes/client.mjs
+++ routes/client.mjs
@@ -61,4 +61,56 @@ clientRouteur.put("/:id", async (req, res) => {
   }
 });
 
+clientRouteur.get("/:id/commandes", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const query = `
+      SELECT 
+        c.id AS id_commande,
+        c.date_commande,
+        pc.id_produit,
+        p.nom AS produit_nom,
+        p.prix AS produit_prix
+      FROM commandes c
+      LEFT JOIN produits_commandes pc ON c.id = pc.id_commande
+      LEFT JOIN produits p ON pc.id_produit = p.id
+      WHERE c.id_client = ?
+      ORDER BY c.date_commande ASC
+    `;
+
+    const [rows] = await db.query(query, [id]);
+
+    if (rows.length === 0) {
+      return res.status(404).send("No commandes found for this client.");
+    }
+
+    const results = {};
+    rows.forEach((row) => {
+      if (!results[row.id_commande]) {
+        results[row.id_commande] = {
+          id_commande: row.id_commande,
+          date_commande: row.date_commande,
+          produits: [],
+        };
+      }
+
+      if (row.id_produit) {
+        results[row.id_commande].produits.push({
+          id_produit: row.id_produit,
+          nom: row.produit_nom,
+          prix: row.produit_prix,
+        });
+      }
+    });
+
+    const resultsArray = Object.values(results);
+
+    res.send(resultsArray);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send("An error occurred while fetching commandes of client.");
+  }
+});
 export default clientRouteur;
